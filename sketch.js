478e8843fceb7c0c0905174e4c06106eb38f742b
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-03-31 19:04:45 +0000

    deploy: f1f60116f1368a7c581042037f877ff68de068a7

--- sketch.js
+++ sketch.js
@@ -7,7 +7,35 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(600,600);
+    //Canvas is only as large as the image is
+    createCanvas(200,150);
     background(220);
     image(img,0,0);
+}
+
+var x = 0;
+var y = 0;
+/**
+ * How many cycles it should render per frame
+ */
+const frameCycles = 1000;
+
+const size = 10;
+
+function draw() {
+    var cycles = 0;
+    noStroke();
+    rectMode(CENTER);
+    while(cycles < frameCycles && y < 150) {
+        while(cycles < frameCycles && x < 500) {
+            fill(img.get(x,y));
+            ellipse(x,y,size+5,size+5);
+            cycles++;
+            x+=size;
+        }
+        if(x >= 500) {
+            x = 0;
+            y += size;
+        }
+    }
 }
